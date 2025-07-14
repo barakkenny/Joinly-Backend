f004758797cdd8267d0704d3292000917a0a7352
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-07-14 14:47:33 +0100

    Added logic for users to get added to the room and their records to be stored

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,5 +1,8 @@
 import { Socket } from "socket.io";
 import { v4 as UUIDv4 } from "uuid"
+import IRoomParams from "../interfaces/IRoomParams";
+
+const rooms: Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
 
@@ -7,16 +10,29 @@ const roomHandler = (socket: Socket) => {
         const roomId = UUIDv4()
 
         socket.join(roomId)
+
+        rooms[roomId] = [];
         socket.emit("room-created", { roomId })
 
-        console.log('room created with id', roomId);
+        console.log('room created with id', rooms);
     }
 
-    const joinedRoom = ({ roomId, peerId } : {roomId: string, peerId: string}) => {
+    const joinedRoom = ({ roomId, peerId } : IRoomParams) => {
+        console.log('joined room call', roomId)
+        if(rooms[roomId]) {
+            
         console.log('new user joined the room', roomId, "with peer id as", peerId)
+         rooms[roomId].push(peerId);
+         console.log('added peer to room', rooms)
+         socket.join(roomId);
+
+         socket.emit("get-users", {
+            roomId,
+            participants: rooms[roomId]
+         })
     }
 
-
+    }
     socket.on("create-room", createRoom);
     socket.on("joined-room", joinedRoom);
 }
